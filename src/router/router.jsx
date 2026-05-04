fc26e67aa09c83fdce05579f1d0dd3005ddbc216
leandro291 <lenadrito1@gmail.com>
2026-05-03 21:26:20 -0500

    routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,34 +1,58 @@
 import { createBrowserRouter } from "react-router";
 
 import { Home } from "../app/home/home";
-import { AboutMe } from "../app/about-me/about-me";
 import { DashboardLayout } from "../common/layouts/dashboard-layout";
 import todo from "../app/todo/todo";
-import { TodoDescription } from "../app/todo/todo-description"
+import { Component } from "react";
+import { RickAndMorty } from "../app/rick-and-morty/rick-and-morty";
+import { Character } from "../app/rick-and-morty/character";
+import { Pokemon } from "../app/pokemon/pokemon";
+import ZustandExample from "../app/zustand-example/zustand-example";
+import { Login } from "../app/login/login";
+import { ProtectedRoute } from "../features/pages/auth/components/protected-route";
 
 
 
 export const router = createBrowserRouter([
+
   {
-    path: "/",
-    Component: DashboardLayout,
+    path: "/login",
+    Component: Login
+  },
+
+  {
+    element: <ProtectedRoute />,
     children: [
       {
-        index: true,
-        Component: Home,
+        path: "/",
+        Component: DashboardLayout,
+        children: [
+          {
+            index: true,
+            Component: Home,
+          },
+          {
+            path: "todo",
+            Component: todo,
+          },
+          {
+            path: "zustand",
+            Component: ZustandExample,
+          },
+          {
+            path: "rick-and-morty",
+            Component: RickAndMorty 
+          },
+          {
+            path: "/character/:characterId",
+            Component: Character 
+          },
+          {
+            path: "pokemon",
+            Component: Pokemon
+          }
+        ],
       },
-      {
-        path: "about-me",
-        Component: AboutMe,
-      },
-      {
-        path: "todo",
-        Component: todo,
-      },
-      {
-        path: "description",
-        Component: TodoDescription
-      },
-    ],
+    ]
   },
 ]);
